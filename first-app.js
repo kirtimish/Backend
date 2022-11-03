4534c6a08b2430fc84eb6a922dadaeb9e4e434d0
kirtimish <kirtimishra0708@gmail.com>
2022-11-04 01:02:38 +0530

    with req and res

--- first-app.js
+++ first-app.js
@@ -4,7 +4,34 @@
 const http = require('http');
 
 const server = http.createServer((req,res) => {
-    console.log('Kirti');
+    if(req.url === '/home'){
+        console.log(req.url, req.method, req.headers);
+        // process.exit();
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title> my first web page</title></head>')
+        res.write('<body><h1>Welcome to Home</h1></body>');
+        res.write('</html>')
+        res.end();
+    } else if(req.url === '/about'){
+        console.log(req.url, req.method, req.headers);
+        // process.exit();
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title> my first web page</title></head>')
+        res.write('<body><h1>Welcome to my About us page</h1></body>');
+        res.write('</html>')
+        res.end();
+    } else{
+        console.log(req.url, req.method, req.headers);
+        // process.exit();
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title> my first web page</title></head>')
+        res.write('<body><h1>Welcome to my Node Js project</h1></body>');
+        res.write('</html>')
+        res.end();
+    }
 })
 
 server.listen('4000');
