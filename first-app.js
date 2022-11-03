0e33dc38195fc10f9eab47e229caac837c4fd055
kirtimish <kirtimishra0708@gmail.com>
2022-11-04 00:08:26 +0530

    httpserver added

--- first-app.js
+++ first-app.js
@@ -1,4 +1,10 @@
-console.log('Hello World')
 // const fs = require('fs');
 
 // fs.writeFileSync('hello.txt','hello from hello txt');
+const http = require('http');
+
+const server = http.createServer((req,res) => {
+    console.log('Kirti');
+})
+
+server.listen('4000');
