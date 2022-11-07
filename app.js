b71bcefe879effdb33530249f1b039cdf5994227
kirtimish <kirtimishra0708@gmail.com>
2022-11-08 03:42:42 +0530

    WithExpressJs

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-const http = require('http');
-const routes = require('./routes');
+const express = require('express');
+const app = express();
 // console.log(routes.someText);
 
-const server = http.createServer(routes);
-// const server = http.createServer(routes.handler);
+app.use((req,res,next) => {
+    console.log('in the middleware');
+    next();
+})
 
-server.listen('4000');
+app.use((req,res,next) => {
+    console.log('in another middleware');
+    res.send("<h1>Hello from express</h1>")
+})
+
+
+app.listen('3000');
