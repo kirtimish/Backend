4bcdd81071a7cf9f9e402d9717859ffc8a98b4cc
kirtimish <kirtimishra0708@gmail.com>
2022-11-09 01:41:20 +0530

    Simple Chat Project

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const loginRoutes = require('./routes/login');
+const messageRoutes = require('./routes/message');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended:false}));
 
 app.use('/admin',adminRoutes);
+
+app.use(loginRoutes);
+app.use(messageRoutes);
 
 app.use('/shop',shopRoutes);
 
---
+++ routes/message.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const fs = require('fs');
+const router = express.Router();
+
+router.get('/',(req,res,next) => {
+    fs.readFile('message.txt',(err,data) => {
+        res.send(`${data}<form onsubmit="document.getElementById('userName').value=localStorage.getItem('username')" action="/" method="POST">
+        <input type="text" name="username">
+        <input type="hidden" name="username" id="userName">
+        <input type="text" name="msg">
+        <button type="submit">Send</button>
+        </form>`);
+    })
+})
+
+router.post('/',(req,res,next) => {
+    fs.writeFile('message.txt', `${req.body.username} : ${req.body.msg}`, {flag: 'a'} , (err) => {
+        err ? console.log(err) : res.redirect('/');
+    })
+})
+
+module.exports = router;
